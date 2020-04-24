efd3c23a7ca80084a41f3884c0bb7759b0395800
parzuko <jivansh17@gmail.com>
2020-04-24 15:49:28 +0530

    First Commit

---
+++ app.py
@@ -0,0 +1,25 @@
+class CreatePlaylist():
+
+    def init(self):
+        pass
+    
+    
+    #log into youtube
+    def get_youtube_client(self):
+        pass
+
+    #get liked vids
+    def get_liked_vids(self):
+        pass
+
+    #create playlist
+    def create_playlist(self):
+        pass
+
+    #search song 
+    def get spotify_url(self):
+        pass
+
+    #add song to playlist
+    def add_song_to_playlist(self):
+        pass
